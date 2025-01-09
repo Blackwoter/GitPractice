ab95abb03c545d8e47d1e12368248af78764ae95
a126 <105658927+Blackwoter@users.noreply.github.com>
2025-01-09 20:00:00 +0000

    deleted 1line

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from PIL import ImageGrab
 
 bot = telebot.TeleBot("7597921187:AAGMGKrSSJ8f4ofggI_BRBpaIKQ8J8mtCgs")
 
-bot.send_message(1348538247, f"Компьютер запущен.\n/start")
 
 Button_1 = types.InlineKeyboardButton(text = 'Снимок экрана', callback_data = 'screen')
 Button_2 = types.InlineKeyboardButton(text = 'Выключить компьютер.', callback_data = 'pc_off')
@@ -20,7 +19,6 @@ def back_welcome(call):
     mark = types.InlineKeyboardMarkup()
     mark.add(Button_1, Button_2)
     bot.send_message(call.message.chat.id, "Компьютер запущен.", reply_markup = mark)
-
 
 
 def pc_off(call):
